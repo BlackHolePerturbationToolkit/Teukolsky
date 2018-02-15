4eaf78731be85ae79bd749a06b4748d22316c3fc
Barry Wardell <barry.wardell@gmail.com>
2018-02-15 11:30:09 +0000

    Add functions for computing first derivatives of MST solutions

--- MST.m
+++ MST.m
@@ -47,6 +47,22 @@ H2F1Down[n_, s_, \[Nu]_, \[Tau]_, \[Epsilon]_, x_] :=
     -((((n + \[Nu]) + 2 - (s + I \[Epsilon])) ((n + \[Nu]) + 2 - I \[Tau]))/(2 + n + \[Nu]) ) H2F1[n + 2]
   };
 
+(* FIXME: There's probably a better way to write this using identities for derivaties of 2F1 *)
+dH2F1Exact[n_, s_, \[Nu]_, \[Tau]_, \[Epsilon]_, x_] := 
+  Derivative[0, 0, 0, 1][Hypergeometric2F1][n + \[Nu] + 1 - I \[Tau], -n - \[Nu] - I \[Tau], 1 - s - I (\[Epsilon] + \[Tau]), x];
+
+dH2F1Up[n_, s_, \[Nu]_, \[Tau]_, \[Epsilon]_, x_] :=
+  (n + \[Nu])/((n + \[Nu] - (s + I \[Epsilon])) ((n + \[Nu]) - I \[Tau])) {
+   -(((-1 + n + s + I \[Epsilon] + \[Nu]) (-1 + n + \[Nu] + I \[Tau]) dH2F1[n - 2])/(-1 + n + \[Nu])),
+   (-1 + 2 (n + \[Nu])) (-2 H2F1[n - 1]),
+   (-1 + 2 (n + \[Nu])) (1 - 2 x + (I (s + I \[Epsilon]) \[Tau])/((-1 + n + \[Nu]) (n + \[Nu]))) dH2F1[n - 1]};
+
+dH2F1Down[n_, s_, \[Nu]_, \[Tau]_, \[Epsilon]_, x_] :=
+  (1 + n + \[Nu])/((1 + n + s + I \[Epsilon] + \[Nu]) (1 + n + \[Nu] + I \[Tau])) {
+    (3 + 2 (n + \[Nu])) (-2 H2F1[n + 1]),
+    (3 + 2 (n + \[Nu])) (1 - 2 x + (I (s + I \[Epsilon]) \[Tau])/((1 + n + \[Nu]) (2 + n + \[Nu]))) dH2F1[n + 1],
+    -(((2 + n - s - I \[Epsilon] + \[Nu]) (2 + n + \[Nu] - I \[Tau]) dH2F1[n + 2])/(2 + n + \[Nu]))};
+
 HUExact[n_, s_, \[Nu]_, \[Epsilon]_, zhat_] :=
   E^(I zhat) (-2 I zhat)^(n + \[Nu] + 1) HypergeometricU[n + \[Nu] + 1 + s - I \[Epsilon], 2 (n + \[Nu]) + 2, -2 I zhat];
 
@@ -61,6 +77,21 @@ HUDown[n_, s_, \[Nu]_, \[Epsilon]_, zhat_] :=
    (((n + \[Nu]) + 1) ((n + \[Nu]) + 2 + (s - I \[Epsilon])))/(((n + \[Nu]) + 2) ((n + \[Nu]) + 1 - (s - I \[Epsilon]))) HU[n + 2]
   };
 
+dHUExact[n_, s_, \[Nu]_, \[Epsilon]_, zhat_] :=
+ 2^(1 + n + \[Nu]) E^(I zhat) (-I zhat)^(n + \[Nu]) (-I (1 + n + I zhat + \[Nu]) HypergeometricU[n + \[Nu] + 1 + s - I \[Epsilon], 2 (n + \[Nu]) + 2, -2 I zhat] - 2 zhat Derivative[0,0,1][HypergeometricU][n + \[Nu] + 1 + s - I \[Epsilon], 2 (n + \[Nu]) + 2, -2 I zhat]);
+
+dHUUp[n_, s_, \[Nu]_, \[Epsilon]_, zhat_] :=
+ 1/(((n + \[Nu]) - 1) ((n + \[Nu]) + s - I \[Epsilon])) {
+   (n + \[Nu]) ((n + \[Nu]) - 1 - (s - I \[Epsilon])) dHU[n - 2],
+   I (-1 + 2 n + 2 \[Nu]) (n - n^2 + \[Nu] - 2 n \[Nu] - \[Nu]^2) HU[n - 1] / zhat^2,
+   I (-1 + 2 n + 2 \[Nu]) (n^2 - I s zhat - zhat \[Epsilon] - \[Nu] + \[Nu]^2 + n (-1 + 2 \[Nu])) dHU[n - 1] / zhat
+ };
+
+dHUDown[n_, s_, \[Nu]_, \[Epsilon]_, zhat_] :=
+ 1 / ((2 + n + \[Nu]) (1 + n - s + I \[Epsilon] + \[Nu])) {
+   -I (3 + 2 (n + \[Nu])) / zhat^2 (-(2 + n^2 + 3 \[Nu] + \[Nu]^2 + n (3 + 2 \[Nu])) HU[n + 1]),
+   -I (3 + 2 (n + \[Nu])) / zhat (2 + n^2 - I s zhat - zhat \[Epsilon] + 3 \[Nu] + \[Nu]^2 + n (3 + 2 \[Nu])) dHU[n + 1],
+   (1 + n + \[Nu]) (2 + n + s - I \[Epsilon] + \[Nu]) dHU[n + 2]};
 
 (******************************************************************************)
 (************************** MST series coefficients ***************************)
@@ -144,6 +175,61 @@ MSTRadialIn[M_, a_, \[Omega]_, s_Integer, l_Integer, m_Integer, \[Nu]_, \[Lambda
   E^(I \[Epsilon] \[Kappa] x) (-x)^(-s - I (\[Epsilon] + \[Tau])/2) (1 - x)^(I (\[Epsilon] - \[Tau])/2) (resUp + resDown)
 ]]];
 
+Derivative[0, 0, 0, 0, 0, 0, 0, 0, 1][MSTRadialIn][M_, a_, \[Omega]_, s_Integer, l_Integer, m_Integer, \[Nu]_, \[Lambda]_, r_?InexactNumberQ] :=
+ Module[{q, \[Epsilon], \[Kappa], \[Tau], rp, z, zp, x, dxdr, resUp, nUp, resDown, nDown},
+ Block[{H2F1, dH2F1},
+ Internal`InheritedBlock[{\[Alpha], \[Beta], \[Gamma], f},
+  q = a/M;
+  \[Epsilon] = 2 M \[Omega];
+  \[Kappa] = Sqrt[1 - q^2];
+  \[Tau] = (\[Epsilon] - m q)/\[Kappa];
+  rp = M + Sqrt[M^2 - a^2];
+  z = \[Omega] r;
+  zp = \[Omega] rp;
+  x = (zp - z)/(\[Epsilon] \[Kappa]);
+  dxdr = - \[Omega]/(\[Epsilon] \[Kappa]);
+
+  H2F1[n : (0 | 1)] := H2F1[n] = H2F1Exact[n, s, \[Nu], \[Tau], \[Epsilon], x];
+
+  H2F1[n_Integer] := H2F1[n] =
+   Module[{t1, t2, res},
+    {t1, t2} = If[n>0, H2F1Up[n, s, \[Nu], \[Tau], \[Epsilon], x], H2F1Down[n, s, \[Nu], \[Tau], \[Epsilon], x]];
+    res = t1 + t2;
+    If[Max[Abs[{t1, t2}/res]] > 2.,
+      res = H2F1Exact[n, s, \[Nu], \[Tau], \[Epsilon], x];
+    ];
+    res
+  ];
+
+  dH2F1[n : (0 | 1)] := dH2F1[n] = dH2F1Exact[n, s, \[Nu], \[Tau], \[Epsilon], x];
+
+  dH2F1[n_Integer] := dH2F1[n] =
+   Module[{t1, t2, t3, res},
+    {t1, t2, t3} = If[n>0, dH2F1Up[n, s, \[Nu], \[Tau], \[Epsilon], x], dH2F1Down[n, s, \[Nu], \[Tau], \[Epsilon], x]];
+    res = t1 + t2 + t3;
+    If[Max[Abs[{t1, t2, t3}/res]] > 2.,
+      res = dH2F1Exact[n, s, \[Nu], \[Tau], \[Epsilon], x];
+    ];
+    res
+  ];
+ 
+  resUp = resDown = 0;
+
+  nUp = 0;
+  While[resUp != (resUp += 
+    f[q, \[Epsilon], \[Kappa], \[Tau], \[Nu], \[Lambda], s, m, nUp] ((-2 s (-1 + x) + I (\[Epsilon] - 2 x \[Epsilon] \[Kappa] + 2 x^2 \[Epsilon] \[Kappa] + \[Tau] - 2 x \[Tau])) H2F1[nUp] + 2 (-1 + x) x dH2F1[nUp])),
+    nUp++;
+  ];
+
+  nDown = -1;
+  While[resDown != (resDown += 
+    f[q, \[Epsilon], \[Kappa], \[Tau], \[Nu], \[Lambda], s, m, nDown] ((-2 s (-1 + x) + I (\[Epsilon] - 2 x \[Epsilon] \[Kappa] + 2 x^2 \[Epsilon] \[Kappa] + \[Tau] - 2 x \[Tau])) H2F1[nDown] + 2 (-1 + x) x dH2F1[nDown])),
+    nDown--;
+  ];
+
+  1/2 E^(I x \[Epsilon] \[Kappa]) (1 - x)^(1/2 I (2 I + \[Epsilon] - \[Tau])) (-x)^(-1 - s - 1/2 I (\[Epsilon] + \[Tau])) (resUp + resDown) dxdr
+]]];
+
 (* Throwe B.5, Sasaki & Tagoshi (153) and (159) *)
 MSTRadialUp[M_, a_, \[Omega]_, s_Integer, l_Integer, m_Integer, \[Nu]_, \[Lambda]_, r_?InexactNumberQ] := 
  Module[{q, \[Epsilon], \[Kappa], \[Tau], \[Epsilon]p, rm, z, zm, zhat, resUp, nUp, resDown, nDown},
@@ -186,6 +272,62 @@ MSTRadialUp[M_, a_, \[Omega]_, s_Integer, l_Integer, m_Integer, \[Nu]_, \[Lambda
   ];
 
   2^\[Nu] E^(-\[Pi] \[Epsilon]) E^(-I \[Pi](\[Nu]+1+s)) E^(I zhat) zhat^(\[Nu]+I \[Epsilon]p) (zhat-\[Epsilon] \[Kappa])^(-s-I \[Epsilon]p) (resUp + resDown)
+]]];
+
+Derivative[0, 0, 0, 0, 0, 0, 0, 0, 1][MSTRadialUp][M_, a_, \[Omega]_, s_Integer, l_Integer, m_Integer, \[Nu]_, \[Lambda]_, r_?InexactNumberQ] := 
+ Module[{q, \[Epsilon], \[Kappa], \[Tau], \[Epsilon]p, rm, z, zm, zhat, dzhatdr, resUp, nUp, resDown, nDown},
+ Block[{HU, dHU},
+ Internal`InheritedBlock[{\[Alpha], \[Beta], \[Gamma], f},
+  q = a/M;
+  \[Epsilon] = 2 M \[Omega];
+  \[Kappa] = Sqrt[1 - q^2];
+  \[Tau] = (\[Epsilon] - m q)/\[Kappa];
+  \[Epsilon]p = 1/2 (\[Tau]+\[Epsilon]);
+  rm = M - Sqrt[M^2 - a^2];
+  z = \[Omega] r;
+  zm = \[Omega] rm;
+  zhat = z - zm;
+  dzhatdr = \[Omega];
+ 
+  HU[n : (0 | 1)] := HU[n] = HUExact[n, s, \[Nu], \[Epsilon], zhat];
+ 
+  HU[n_Integer] := HU[n] =
+   Module[{t1, t2, res},
+    {t1, t2} = If[n>0, HUUp[n, s, \[Nu], \[Epsilon], zhat], HUDown[n, s, \[Nu], \[Epsilon], zhat]];
+    res = t1 + t2;
+    If[Max[Abs[{t1, t2}/res]] > 2.,
+      res = HUExact[n, s, \[Nu], \[Epsilon], zhat];
+    ];
+    res
+  ];
+ 
+  dHU[n : (0 | 1)] := dHU[n] = dHUExact[n, s, \[Nu], \[Epsilon], zhat];
+ 
+  dHU[n_Integer] := dHU[n] =
+   Module[{t1, t2, t3, res},
+    {t1, t2, t3} = If[n>0, dHUUp[n, s, \[Nu], \[Epsilon], zhat], dHUDown[n, s, \[Nu], \[Epsilon], zhat]];
+    res = t1 + t2 + t3;
+    If[Max[Abs[{t1, t2, t3}/res]] > 2.,
+      res = dHUExact[n, s, \[Nu], \[Epsilon], zhat];
+    ];
+    res
+  ];
+
+  resDown = resUp = 0;
+  nUp = 0;
+
+  While[resUp != (resUp +=
+    I^nUp (-I zhat)^(1 - nUp - \[Nu]) zhat^(-3 + nUp + I \[Epsilon]p + \[Nu]) f[q, \[Epsilon], \[Kappa], \[Tau], \[Nu], \[Lambda], s, m, nUp] Pochhammer[1 + s - I \[Epsilon] + \[Nu], nUp] (((1 + s) zhat + I \[Epsilon] (I + \[Epsilon]p) \[Kappa]) HU[nUp] + zhat (-zhat + \[Epsilon] \[Kappa]) dHU[nUp]) / (2 Pochhammer[1 - s + I \[Epsilon] + \[Nu], nUp])),
+    nUp++;
+  ];
+  
+  nDown = -1;
+  While[resDown != (resDown +=
+    I^nDown (-I zhat)^(1 - nDown - \[Nu]) zhat^(-3 + nDown + I \[Epsilon]p + \[Nu]) f[q, \[Epsilon], \[Kappa], \[Tau], \[Nu], \[Lambda], s, m, nDown] Pochhammer[1 + s - I \[Epsilon] + \[Nu], nDown] (((1 + s) zhat + I \[Epsilon] (I + \[Epsilon]p) \[Kappa]) HU[nDown] + zhat (-zhat + \[Epsilon] \[Kappa]) dHU[nDown]) / (2 Pochhammer[1 - s + I \[Epsilon] + \[Nu], nDown])),
+    nDown--;
+  ];
+
+  E^(-\[Pi] (\[Epsilon] + I (1 + s + \[Nu]))) (zhat - \[Epsilon] \[Kappa])^(-1 - s - I \[Epsilon]p) (resUp + resDown) dzhatdr
 ]]];
 
 End[];
