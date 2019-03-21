bd3383eb8dcd3a7844ccc38c92a2fa3a177c91ef
Niels Warburton <nielsw@gmail.com>
2019-03-21 10:19:14 +0000

    Load all the subpackages when the main package is loaded

--- Kernel/init.m
+++ Kernel/init.m
@@ -5,3 +5,5 @@ Get["Teukolsky`RenormalizedAngularMomentum`"];
 Get["Teukolsky`SasakiNakamura`"];
 Get["Teukolsky`MST`"];
 Get["Teukolsky`TeukolskyRadial`"];
+Get["Teukolsky`TeukolskySource`"];
+Get["Teukolsky`TeukolskyMode`"];
