254fdf8012878effdef0d61617e2731f6bb44028
Niels Warburton <nielsw@gmail.com>
2019-05-10 18:29:18 +0100

    Add the calculation of the horizon flux (and the total flux)

--- TeukolskyMode.m
+++ TeukolskyMode.m
@@ -25,7 +25,7 @@ TeukolskyPointParticleMode[s_Integer, l_Integer, m_Integer, n_Integer, k_Integer
 	TeukolskyPointParticleMode[s,l,m,n,k,orbit,source]
 ]
 
-TeukolskyPointParticleMode[s_Integer, l_Integer, m_Integer, n_Integer, k_Integer, orbit_KerrGeoOrbitFunction, source_TeukolskySourceObject]:=Module[{assoc, R, S, \[Omega], \[CapitalOmega]r, \[CapitalOmega]\[Phi], \[CapitalOmega]\[Theta], Z, a, Fluxes},
+TeukolskyPointParticleMode[s_Integer, l_Integer, m_Integer, n_Integer, k_Integer, orbit_KerrGeoOrbitFunction, source_TeukolskySourceObject]:=Module[{M=1, assoc, R, S, \[Omega], \[CapitalOmega]r, \[CapitalOmega]\[Phi], \[CapitalOmega]\[Theta], Z, a, \[Lambda], AbsCSq, Fluxes, rh, \[Epsilon], \[Kappa], \[CapitalOmega]h, \[Alpha], FluxInf, FluxHor},
 
 (*{\[CapitalOmega]r, \[CapitalOmega]\[Theta], \[CapitalOmega]\[Phi]} = orbit["Frequencies"];*) (*This gives Mino frequencies, need BL frequencies*)
 
@@ -37,12 +37,25 @@ a = orbit[[1]]; (*FIXME: should be able to get parameters from the orbit via ass
 
 R = TeukolskyRadial[s, l, m, a, \[Omega]];  
 
+\[Lambda] = R["Eigenvalue"];
+
 S = SpinWeightedSpheroidalHarmonicS[s, l, m, a \[Omega]];
 
 Z = Teukolsky`ConvolveSource`Private`ConvolveSource[R, S, source];
 
-(*For flux calculation we need to check the normalization*)
-Fluxes = <|"FluxInf" -> Abs[Z["ZInf"]]^2/(4 \[Pi] \[Omega]^2)|>;
+rh = M + Sqrt[M^2-a^2];
+\[CapitalOmega]h = a/(2 M rh);
+\[Kappa] = \[Omega] - m \[CapitalOmega]h;
+\[Epsilon] = Sqrt[M^2-a^2]/(4 M rh);
+
+AbsCSq = ((\[Lambda]+2)^2 + 4 a m \[Omega] - 4a^2 \[Omega]^2)(\[Lambda]^2+36 m a \[Omega] - 36 a^2 \[Omega]^2) + (2\[Lambda]+3)(96 a^2 \[Omega]^2 - 48 m a \[Omega]) + 144 \[Omega]^2 (M^2-a^2);
+
+\[Alpha] = (256(2M rh)^5 \[Kappa](\[Kappa]^2+4\[Epsilon]^2)(\[Kappa]^2+16\[Epsilon]^2)\[Omega]^3)/AbsCSq;
+
+FluxInf =  Abs[Z["ZInf"]]^2/(4 \[Pi] \[Omega]^2);
+FluxHor = \[Alpha] Abs[Z["ZHor"]]^2/(4 \[Pi] \[Omega]^2);
+
+Fluxes = <|"FluxInf" -> FluxInf, "FluxHor" -> FluxHor, "FluxTotal" -> FluxInf + FluxHor |>;
 
 assoc = <| "s" -> s, 
 		   "l" -> l,
