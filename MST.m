7491450f21bafd188ae77d47f795a7930849bcba
Barry Wardell <barry.wardell@gmail.com>
2020-02-28 16:52:38 +0000

    Remove MST usage messages

--- MST.m
+++ MST.m
@@ -2,9 +2,6 @@
 
 BeginPackage["Teukolsky`MST`"];
 
-(*MSTRadialIn::usage = "MSTRadialIn[s, l, m, q, \[Epsilon], \[Nu], \[Lambda]][r]";
-MSTRadialUp::usage = "MSTRadialUp[s, l, m, q, \[Epsilon], \[Nu], \[Lambda]][r]";*)
-
 Begin["`Private`"];
 
 (******************************************************************************)
