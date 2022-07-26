329d0dd6529e981d62981571f8fee63cce619b4f
Kevin-Cunningham <kevincunningham123@gmail.com>
2022-07-26 12:13:26 -0400

    Added code to support generic orbits in future. Also changed way that frequencies are taken from KerrGeoFrequencies, so Values is no longer used

--- Kernel/TeukolskySource.m
+++ Kernel/TeukolskySource.m
@@ -32,11 +32,11 @@ Begin["`Private`"];
 
 TeukolskyPointParticleSource[s_, orbit_] :=
 Module[{},
-  If[(s==-2 || s==-1 || s==+1 || s==0) && orbit["e"] == 0 && Abs[orbit["Inclination"]] == 1,
+  If[(s==-1 || s==+1) && orbit["e"] == 0 && Abs[orbit["Inclination"]] == 1,
     Return[TeukolskyPointParticleSourceCircular[s,orbit]]];
-  If[(s==0||s==-2) && orbit["e"] == 0 && Abs[orbit["Inclination"]] != 1,
-    Return[TeukolskyPointParticleSourceSpherical[s,orbit]]];
-    
+  If[(s==-2||s==0),
+  Switch[{orbit["e"],Abs[orbit["Inclination"]]},{0,1},Return[TeukolskyPointParticleSourceCircular[s,orbit]],{0,x_},Return[TeukolskyPointParticleSourceSpherical[s,orbit]],{e_,1},Return[TeukolskyPointParticleSourceEccentric[s,orbit]],{e_,x_},Return[TeukolskyPointParticleSourceGeneric[s,orbit]]]
+  ];  
     $Failed
     ]
     
@@ -94,6 +94,46 @@ TeukolskyPointParticleSourceSpherical[0, orbit_] := Module[{assoc, \[Alpha], gtt
         "SourceType" -> "PointParticle",
         "Orbit" -> orbit,
         "\[Alpha]" -> \[Alpha]
+      |>;
+
+  TeukolskySourceObject[assoc]
+]
+
+
+TeukolskyPointParticleSourceEccentric[0, orbit_] := Module[{assoc, a, r0, e, x, \[ScriptCapitalE], \[ScriptCapitalL]},
+	r0 =orbit["p"];
+	a= orbit["a"];
+	x = orbit["Inclination"];
+	e = orbit["e"];
+	
+	\[ScriptCapitalE]=orbit["Energy"];
+	\[ScriptCapitalL]=orbit["AngularMomentum"];
+	
+	
+
+  assoc = <|  "s" -> 0,
+        "SourceType" -> "PointParticle",
+        "Orbit" -> orbit
+      |>;
+
+  TeukolskySourceObject[assoc]
+]
+
+
+TeukolskyPointParticleSourceGeneric[0, orbit_] := Module[{assoc, a, r0, e, x, \[ScriptCapitalE], \[ScriptCapitalL]},
+	r0 =orbit["p"];
+	a= orbit["a"];
+	x = orbit["Inclination"];
+	e = orbit["e"];
+	
+	\[ScriptCapitalE]=orbit["Energy"];
+	\[ScriptCapitalL]=orbit["AngularMomentum"];
+	
+	
+
+  assoc = <|  "s" -> 0,
+        "SourceType" -> "PointParticle",
+        "Orbit" -> orbit
       |>;
 
   TeukolskySourceObject[assoc]
@@ -215,6 +255,74 @@ CmmMinus=\[Rho]^2/(2\[CapitalSigma] ut) (I Sin[\[Theta]](a \[ScriptCapitalE]-\[S
 
 
 assoc = <|"s"->-2, "SourceType"->"PointParticle","Orbit"->orbit,"Cnn+"->CnnPlus,"Cnn-"->CnnMinus,"Cnm+"->CnmPlus,"Cnm-"->CnmMinus,"Cmm+"->CmmPlus,"Cmm-"->CmmMinus,"\[Chi]"->\[Chi]|>
+]
+
+
+TeukolskyPointParticleSourceEccentric[-2, orbit_] := Module[{assoc, a,r0, E0, Lz, \[CapitalSigma], Cnn, Cnmb, Cmbmb, ut, \[Rho], \[CapitalDelta], gtt, gt\[Phi]},
+  a = orbit["a"];
+  r0 = orbit["p"];
+
+  (*BEGIN FIXME: this should come from KerrGeoOrbit but currently we don't have code to compute the four-velocity in the KerrGeodesics package*)
+
+  E0 = orbit["Energy"];
+  Lz = orbit["AngularMomentum"];
+
+  \[CapitalSigma] = r0^2;
+  \[Rho] = -1/r0;
+
+  \[CapitalDelta] = r0^2 - 2r0 + a^2;
+  gtt = -(1/\[CapitalSigma])((r0^2+a^2)^2/\[CapitalDelta]-a^2);
+  gt\[Phi] = -((2 r0 a)/(\[CapitalSigma] \[CapitalDelta]));
+  ut = gt\[Phi] Lz - gtt E0;
+  (*END FIXME*)
+
+  Cnn  = 1/(4 \[CapitalSigma]^3 ut) (E0(r0^2+a^2)-a Lz)^2;
+  Cnmb = \[Rho]/(2Sqrt[2]\[CapitalSigma]^2 ut) (E0(r0^2+a^2)-a Lz)(I (a E0 - Lz));
+  Cmbmb = \[Rho]^2/(2 \[CapitalSigma] ut) (I (a E0 - Lz))^2;
+
+  assoc = <| "s" -> -2, 
+         "SourceType" -> "PointParticle",
+         "Orbit" -> orbit,
+         "Cnn" -> Cnn,
+         "Cnmb" -> Cnmb,
+         "Cmbmb" -> Cmbmb
+         |>;
+
+  TeukolskySourceObject[assoc]
+]
+
+
+TeukolskyPointParticleSourceGeneric[-2, orbit_] := Module[{assoc, a,r0, E0, Lz, \[CapitalSigma], Cnn, Cnmb, Cmbmb, ut, \[Rho], \[CapitalDelta], gtt, gt\[Phi]},
+  a = orbit["a"];
+  r0 = orbit["p"];
+
+  (*BEGIN FIXME: this should come from KerrGeoOrbit but currently we don't have code to compute the four-velocity in the KerrGeodesics package*)
+
+  E0 = orbit["Energy"];
+  Lz = orbit["AngularMomentum"];
+
+  \[CapitalSigma] = r0^2;
+  \[Rho] = -1/r0;
+
+  \[CapitalDelta] = r0^2 - 2r0 + a^2;
+  gtt = -(1/\[CapitalSigma])((r0^2+a^2)^2/\[CapitalDelta]-a^2);
+  gt\[Phi] = -((2 r0 a)/(\[CapitalSigma] \[CapitalDelta]));
+  ut = gt\[Phi] Lz - gtt E0;
+  (*END FIXME*)
+
+  Cnn  = 1/(4 \[CapitalSigma]^3 ut) (E0(r0^2+a^2)-a Lz)^2;
+  Cnmb = \[Rho]/(2Sqrt[2]\[CapitalSigma]^2 ut) (E0(r0^2+a^2)-a Lz)(I (a E0 - Lz));
+  Cmbmb = \[Rho]^2/(2 \[CapitalSigma] ut) (I (a E0 - Lz))^2;
+
+  assoc = <| "s" -> -2, 
+         "SourceType" -> "PointParticle",
+         "Orbit" -> orbit,
+         "Cnn" -> Cnn,
+         "Cnmb" -> Cnmb,
+         "Cmbmb" -> Cmbmb
+         |>;
+
+  TeukolskySourceObject[assoc]
 ]
 
 
