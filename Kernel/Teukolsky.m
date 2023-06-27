1986593d004d566aa5a84da1e75ffe19a5bf6f02
Barry Wardell <barry.wardell@gmail.com>
2023-06-27 17:49:05 +0100

    Check appropriate versions of KerrGeodesics and SpinWeightedSpheroidalHarmonics are installed

--- Kernel/Teukolsky.m
+++ Kernel/Teukolsky.m
@@ -2,6 +2,23 @@
 
 BeginPackage["Teukolsky`"];
 
+Begin["`Private`"];
+
+(* Check appropriate versions of dependencies are installed *)
+versionCheck[name_, version_] :=
+ Module[{paclet},
+  paclet = PacletObject[name];
+  If[!(PacletNewerQ[paclet, version] || paclet["Version"] == version),
+    Throw[Failure["PacletNotFound",
+      <|"MessageTemplate" ->"Paclet `1` version `2` or greater not found.", 
+        "MessageParameters" -> {name, version}|>], Teukolsky, #1&]
+  ]
+];
+versionCheck["SpinWeightedSpheroidalHarmonics", "1.0.0"];
+versionCheck["KerrGeodesics", "0.9.0"];
+
+End[];
+
 EndPackage[];
 
 Block[{MST`$MasterFunction = "Teukolsky"},
